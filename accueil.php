a5c22e099b01733952abd3a61107bfbaf8d2e830
Antoine62170 <56411808+Antoine62170@users.noreply.github.com>
2019-10-15 17:05:37 +0200

    Add files via upload

---
+++ accueil.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+</head>
+<meta charset="utf-8" />
+<link rel="stylesheet" href="multi.css">
+<title>Table de multiplication</title>
+</head>
+<body>
---
+++ accueil_form.php
@@ -0,0 +1,11 @@
+<html lang="fr" xmlns="http://www.w3.org/1999/xhtml">
+<form method = "post" action="salarie.php">
+<html> 
+    <h1> Avalon </h1> </html>
+
+Nom <input type="text" name="nom" value=""><br>
+Prenom <input type="text" name="prenom" value=""><br>
+Adresse mail <input type="text" name="mail" value=""><br>
+Garage d'affectation <select name="numero">
+
+</html>
